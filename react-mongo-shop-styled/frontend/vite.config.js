1aba44195e2eaa581cfc70f3e01afc90d8b81df9
eleonoraivanova0508-spec <eleonoraivanova0508@gmail.com>
2025-12-28 21:47:15 +0500

    Update vite.config.js

--- react-mongo-shop-styled/frontend/vite.config.js
+++ react-mongo-shop-styled/frontend/vite.config.js
@@ -6,7 +6,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    port: 5173
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
   }
 });
